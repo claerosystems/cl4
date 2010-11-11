44599e583a19bf697dd984fac570206e10784e7f
Craig Nakamoto <craig.nakamoto@claero.com>
2010-11-11 14:38:45 +0000

    splitting files

--- views/claero/base/development_message.php
+++
@@ -1,3 +0,0 @@
-<?php if (DEVELOPMENT_FLAG) { ?>
-<div class="development_msg">This Site is Currently Under Development</div>
-<?php } // if ?>
--- views/claero/base/footer.php
+++
@@ -1,5 +0,0 @@
-	<footer>
-		<nav>
-			&copy;<?php echo date('Y'); ?> Claero Systems <?php echo HTML::chars(__('All Rights Reserved')); ?>
-		</nav>
-	</footer>
