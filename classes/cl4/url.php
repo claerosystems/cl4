7e6c2f80718db7eef40c3cd02fc9a4a4b2de79cf
Darryl Hein <dhein@xmmedia.com>
2010-11-15 04:21:57 +0000

    removing unnecessary function overriding a kohana function (URL::query())

--- classes/cl4/url.php
+++ classes/cl4/url.php
@@ -7,45 +7,6 @@
  * @copyright  (c) 2010 Claero Systems
  */
 class cl4_URL extends Kohana_URL {
-    /**
-	 * Merges the current GET parameters with an array of new or overloaded
-	 * parameters and returns the resulting query string.
-	 *
-	 *     // Returns "?sort=title&limit=10" combined with any existing GET values
-	 *     $query = URL::query(array('sort' => 'title', 'limit' => 10));
-	 *
-	 * Typically you would use this when you are sorting query results,
-	 * or something similar.
-	 *
-	 * [!!] Parameters with a NULL value are left out.
-	 *
-	 * @param   array   array of GET parameters
-	 * @param	bool	if set to true then the GET parameters will be merged with the passed parameters first
-	 * @param	string	the separator to use in http_build_query
-	 * @return  string
-	 */
-	public static function query(array $params = NULL, $merge_get = TRUE, $separator = '&') {
-		if ($merge_get) {
-			if ($params === NULL) {
-				// Use only the current parameters
-				$params = $_GET;
-			} else {
-				// Merge the current and new parameters
-				$params = array_merge($_GET, $params);
-			}
-		}
-
-		if (empty($params)) {
-			// No query parameters
-			return '';
-		}
-
-		$query = http_build_query($params, '', $separator);
-
-		// Don't prepend '?' to an empty string
-		return ($query === '') ? '' : '?' . $query;
-	} // function
-
 	/**
 	* Runs http_build_query() with some defaults
 	* If the resulting string is not empty, the string will include the ? in front
