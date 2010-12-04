76199d9eb1676fceea5c1893c2c69a03a8cb98df
Darryl Hein <dhein@xmmedia.com>
2010-12-04 05:15:03 +0000

    changing the way the options are merged (to a simpler method) updating comments

--- classes/cl4/orm/date.php
+++ classes/cl4/orm/date.php
@@ -5,7 +5,7 @@ class cl4_ORM_Date extends ORM_FieldType {
 	 * @const string The format to output dates as.
 	 */
 	const TIMESTAMP_FORMAT = 'M j, Y';
-	
+
 	public static function edit($column_name, $html_name, $value, array $attributes = NULL, array $options = array(), ORM $orm_model = NULL) {
 		return Form::date($html_name, $value, $attributes);
 	}
@@ -105,9 +105,9 @@ class cl4_ORM_Date extends ORM_FieldType {
 	 * Convert a date of some form to a human-viewable value.
 	 *
 	 * @param mixed $value     The timestamp or MySQL DateTime or descriptive text indicating the time desired.
-	 * @param ORM   $orm_model Unused currently.
-	 * @param array $options   Unused currently.
-	 * @param mixed $source    Unused currently.
+	 * @param ORM   $orm_model The ORM Model, although not used within the function
+	 * @param array $options   Options to be used within the method (none currently)
+	 * @param mixed $source    Unused in ORM_Date
 	 *
 	 * @return string
 	 */
@@ -122,16 +122,16 @@ class cl4_ORM_Date extends ORM_FieldType {
 	 * @param ORM     $orm_model       Unused currently.
 	 * @param array   $options         Options for how to format for HTML:
 	 * @param boolean $options['nbsp']  - If true, replace spaces with "&nbsp;".
-	 * @param mixed   $source          Unused currently.
+	 * @param mixed   $source          Unused in ORM_Date
 	 *
 	 * @return string
 	 */
 	public static function view_html($value, $column_name, ORM $orm_model = NULL, array $options = array(), $source = NULL) {
-		// Set a default for space-for-nbsp replacement
-		if ( ! isset($options['nbsp'])) {
-			$options['nbsp'] = false;
-		}
-		
+		// set some default options
+		$options += array(
+			'nbsp' => FALSE,
+		);
+
 		return ORM_Date::prepare_html(ORM_Date::view($value, $column_name, $orm_model, $options), $options['nbsp']);
 	}
 } // class
