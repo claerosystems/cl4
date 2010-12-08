34ebace58b09c09400effdcbf4b5d4f80d667485
Darryl Hein <dhein@xmmedia.com>
2010-12-08 23:56:38 +0000

    ensuring no value is displayed in the password confirm field

--- classes/cl4/orm/passwordconfirm.php
+++ classes/cl4/orm/passwordconfirm.php
@@ -2,7 +2,7 @@
 
 class cl4_ORM_PasswordConfirm extends cl4_ORM_Password {
 	public static function edit($column_name, $html_name, $value, array $attributes = NULL, array $options = array(), ORM $orm_model = NULL) {
-		return Form::password_confirm($html_name, $value, $attributes, $options);
+		return Form::password_confirm($html_name, '', $attributes, $options);
 	}
 
 	public static function search($column_name, $html_name, $value, array $attributes = NULL, array $options = array(), ORM $orm_model = NULL) {
