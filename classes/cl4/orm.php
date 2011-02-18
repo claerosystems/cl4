58c0fb9d1a01c2b9a3eeb859b3df5ea5bd2298c2
Darryl Hein <dhein@xmmedia.com>
2011-02-17 18:25:10 -0700

    fixing the loading of values when the id is passed in the post and using save_values()

--- classes/cl4/orm.php
+++ classes/cl4/orm.php
@@ -1481,7 +1481,9 @@ class cl4_ORM extends Kohana_ORM {
 
 		// get the id from the post and set it in the object (if there is one, won't be one in 'add' case)
 		if ( ! empty($post[$this->_primary_key])) {
-			$this->clear()->find($post[$this->_primary_key]);
+			$this->clear();
+			$this->where($this->_table_name . '.' . $this->_primary_key, '=', $post[$this->_primary_key])->find();
+
 			// remove the id as we don't want to risk changing it
 			unset($post[$this->_primary_key]);
 		} // if
