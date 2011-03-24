f971ea3678f509a907077ad91f4c2c950669809e
Darryl Hein <dhein@xmmedia.com>
2011-03-24 02:45:40 -0600

    adding AJAX 404 error constant; adding more documentation

--- classes/cl4/ajax/status.php
+++ classes/cl4/ajax/status.php
@@ -1,6 +1,9 @@
 <?php defined('SYSPATH') or die ('No direct script access.');
 
 /**
+* The object i del help dealing with AJAX calls and things like logged out and no access
+* The js/ajax.js in the docroot model on how this is dealt with
+*
 * AJAX JSON structure:
 *
 * array(
@@ -12,11 +15,30 @@
 * )
 */
 class cl4_AJAX_Status {
+	/**
+	* @var  int  0: An unkown error occured during the AJAX request
+	*/
 	const UNKNOWN_ERROR = 0;
+	/**
+	* @var  int  1: The AJAX request was successful
+	*/
 	const SUCCESSFUL = 1;
+	/**
+	* @var  int  2: The user is not logged in
+	*/
 	const NOT_LOGGED_IN = 2;
+	/**
+	* @var  int  3: The user session/login has timed out
+	*/
 	const TIMEDOUT = 3;
+	/**
+	* @var  int  4: The user is not allowed to access the page requested
+	*/
 	const NOT_ALLOWED = 4;
+	/**
+	* @var  int  5: The page/path/URL cannot be found
+	*/
+	const NOT_FOUND_404 = 5;
 
 	/**
 	* JSON encodes the passed array
