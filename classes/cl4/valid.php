1edfbf6ad6792de27fef2313cf3dc4aeb324075d
Darryl Hein <dhein@xmmedia.com>
2011-06-10 22:45:16 -0600

    adding a validation check "select" which checks if the value is >0 useful on fields like radios or selects

---
+++ classes/cl4/valid.php
@@ -0,0 +1,13 @@
+<?php defined('SYSPATH') or die ('No direct script access.');
+
+class cl4_Valid extends Kohana_Valid {
+	/**
+	 * Checks if a value been selected for a field, such as radios or a checkbox.
+	 *
+	 * @return  boolean
+	 */
+	public static function selected($value) {
+		// Value must be > 0
+		return $value > 0;
+	}
+}
---
+++ messages/validation.php
@@ -0,0 +1,5 @@
+<?php defined('SYSPATH') or die ('No direct script access.');
+
+return array(
+	'selected'         => 'A value must be selected for :field',
+);
