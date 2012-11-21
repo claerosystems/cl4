71075b8a517f7c5bd2b36e15b8d331528d17334e
Darryl Hein <dhein@xmmedia.com>
2012-11-21 01:18:23 -0700

    removing the unused production error config param

--- config/cl4.php
+++ config/cl4.php
@@ -3,5 +3,4 @@
 return array(
 	// if in production (based on Kohana::$environment), then email the errors including the HTML view including the trace
 	'email_exceptions' => TRUE,
-);	// view used when there is an error in production
-	'production_error_view' => 'cl4/production_error',
+);
