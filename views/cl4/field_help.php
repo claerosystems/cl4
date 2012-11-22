e56b7727cb2090d8b711b43c64d047b59cf2054b
Darryl Hein <dhein@xmmedia.com>
2012-11-22 10:54:33 -0700

    adding the span element for the help icon on the field help

--- views/cl4/field_help.php
+++ views/cl4/field_help.php
@@ -1,4 +1,4 @@
 <div<?php echo HTML::attributes(array(
 	'class' => 'cl4_field_help cl4_field_help_' . $mode,
 	'data-cl4_field' => $field_html_name,
-)); ?>><?php echo $field_help; ?></div>
+)); ?>><span class="cl4_icon cl4_help"></span><?php echo $field_help; ?></div>
