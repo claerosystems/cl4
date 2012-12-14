de92a20d0264de7ccbe3ef2152f2637a7bf6ab2e
Darryl Hein <dhein@xmmedia.com>
2012-12-14 14:45:50 -0700

    fixing the TRUE in the model create filters
    also changing the layout of the code in filters and rules

--- classes/CL4/Model/Create.php
+++ classes/CL4/Model/Create.php
@@ -546,7 +546,9 @@ class CL4_Model_Create {
 		$model_code .= TAB . '*/' . EOL;
 		$model_code .= TAB . '/*public function rules() {' . EOL;
 		$model_code .= TAB . TAB . 'return array(' . EOL;
-		$model_code .= TAB . TAB . TAB . '\'' . $this->first_text_column_name . '\' => array(array(\'not_empty\')),' . EOL;
+		$model_code .= TAB . TAB . TAB . '\'' . $this->first_text_column_name . '\' => array(' . EOL;
+		$model_code .= TAB . TAB . TAB . TAB . 'array(\'not_empty\'),' . EOL;
+		$model_code .= TAB . TAB . TAB . '),' . EOL;
 		$model_code .= TAB . TAB . ');' . EOL;
 		$model_code .= TAB . '}*/' . EOL;
 
@@ -562,7 +564,9 @@ class CL4_Model_Create {
 		$model_code .= TAB . '*/' . EOL;
 		$model_code .= TAB . '/*public function filters() {' . EOL;
 		$model_code .= TAB . TAB . 'return array(' . EOL;
-		$model_code .= TAB . TAB . TAB . '\'' . ( ! empty($this->first_text_column_name) ? $this->first_text_column_name : 'TRUE') . '\' => array(array(\'trim\')),' . EOL;
+		$model_code .= TAB . TAB . TAB . ( ! empty($this->first_text_column_name) ? '\'' . $this->first_text_column_name . '\'' : 'TRUE') . ' => array(' . EOL;
+		$model_code .= TAB . TAB . TAB . TAB . 'array(\'trim\'),' . EOL;
+		$model_code .= TAB . TAB . TAB . '),' . EOL;
 		$model_code .= TAB . TAB . ');' . EOL;
 		$model_code .= TAB . '}*/' . EOL;
 
