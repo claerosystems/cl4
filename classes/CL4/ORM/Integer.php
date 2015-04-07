cbfd9a70f66b1af40732aa818b0064e78d891c56
Craig Nakamoto <craig.nakamoto@claero.com>
2015-04-07 15:41:02 -0400

    add integer type to use IOS keypad

--- classes/CL4/ORM/Integer.php
+++ classes/CL4/ORM/Integer.php
@@ -1,15 +1,17 @@
 <?php defined('SYSPATH') OR die('No direct access allowed.');
 
 /**
- * Effectively a text input with the type = "number", forces IOS keypad
+ * Effectively a text input with the type = "number", uses IOS keypad, forces Non-negative integral number
  */
-class CL4_ORM_Number extends CL4_ORM_Text {
+class CL4_ORM_Integer extends CL4_ORM_Text {
 	public static function edit($column_name, $html_name, $value, array $attributes = NULL, array $options = array(), ORM $orm_model = NULL) {
 		if (empty($attributes)) {
 			$attributes = array();
 		}
 		// <input type="number" min="0" inputmode="numeric" pattern="[0-9]*" title="Non-negative integral number">
+		$attributes['min'] = 0;
 		$attributes['inputmode'] = 'numeric';
-        return Form::number($html_name, $value, $attributes);
+		$attributes['pattern'] = '[0-9]*';
+		return Form::number($html_name, $value, $attributes);
 	}
 } // class
